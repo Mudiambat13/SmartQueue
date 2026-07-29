3dede2a113cebfe5aade1914b208eb236c0b5f4d
Jonathan Mudiambat <103556538+Mudiambat13@users.noreply.github.com>
2026-07-29 16:03:37 +0200

    Update Ticket.cs

--- SmartQueue.Server/Models/Ticket.cs
+++ SmartQueue.Server/Models/Ticket.cs
@@ -3,8 +3,18 @@ namespace SmartQueue.Server.Models
     public class Ticket
     {
         public string Number { get; set; } = string.Empty;
+
         public string Counter { get; set; } = string.Empty;
-        public DateTime CalledAt { get; set; } = DateTime.Now;
-        public string Status { get; set; } = "Waiting"; // Waiting, Called, Completed
+
+        public DateTime CalledAt { get; set; } = DateTime.UtcNow;
+
+        public TicketStatus Status { get; set; } = TicketStatus.Waiting;
+    }
+
+    public enum TicketStatus
+    {
+        Waiting,
+        Called,
+        Completed
     }
 }
